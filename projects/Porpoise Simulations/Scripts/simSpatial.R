03585578ac16e89ba551449a1df81064519a3349
eirenjacobson <eiren.jacobson@gmail.com>
2017-04-21 16:22:51 -0700

    Created simSpatial script to hold function that generates PPS data given some decline etc.  Also added code to generate a GIF of the generated PPS vals over the whole study area.  Some RData files moved/added from other directory; misc. other changes.

---
+++ projects/Porpoise Simulations/Scripts/simSpatial.R
@@ -0,0 +1,114 @@
+# simSpatial generates spatially explicit PPS values 
+# for n moorings over y years given some rate of change
+# if d.min and d.max are not supplied, default 
+
+simSpatial <- function(n, y, r, d.min=0, d.max=-1000){
+  
+  # n is the number of moorings
+  # y is the number of years
+  # r is the growth rate over the entire period (e.g., for a 50% decline r = -0.5)
+  
+  # d.min and d.max are the depth boundaries for mooring deployment
+  # default d.min and d.max include the entire range of the spatial density obj.
+  # note depth is negative, so default d.min=0 and d.max=-1000
+  
+  # b is whether decline is applied uniformly over the study area (b=0; default)
+  # or whether the decline results in contraction to core habitat (b=1)
+  
+  load("./Data/predGrid.RData") #gridOut created by spDensity
+  load("./Data/Dmodel.RData")
+  
+  gridOut$Q <- gridOut$PpSqKm/sum(gridOut$PpSqKm)
+  
+  # Choose random X, Y locations for n moorings and get D vals
+  gO <- gridOut[which(gridOut$D>d.max & gridOut$D <= d.min),]
+  gLocs <- gO[sample(1:nrow(gO), n, replace=FALSE),]
+  
+  # Generate single random intercept term 
+  int <- D.model$coefficients[1]
+  i.se <- summary(D.model)$coefficients[4]
+  i <- rnorm(1, int, i.se/sqrt(30))
+  
+  # Generate underlying densities for n spatial locs
+  # using the mean and SE of the density surface
+  d.n <- rnorm(n, gLocs$PpSqKm, gLocs$SE.PpSqKm)
+  
+  # Generate overall effect of density on PPS
+  d.eff <- summary(D.model)$coefficients[3]
+  d.se <- summary(D.model)$coefficients[6]
+  b1 <- rnorm(1, d.eff, d.se/sqrt(30))
+  
+  # Add noise to overall rate of change in population over time
+  if (r == 0) {cum.r <- rep(1, y)} else {
+    rvals <- rnorm(y-2, mean=((1+r)^(1/(y-1))), sd=0.05) # sd is arbitrary
+    rvals[y-1] <- (1+r)/prod(rvals[1:(y-2)]) # solve for final decline
+    rvals <- rvals[sample(1:(y-1), (y-1))] 
+    cum.r <- c(1, cumprod(rvals))}
+  
+  sdev <- sigma(D.model)
+  
+  df <- expand.grid("MOORING" = 1:n, 
+                    "YEAR" = 1:y, 
+                    "DENSITY" = NA, 
+                    "PPS"= NA)
+  df$DENSITY <- exp(rep(d.n, y)) # underlying mean density in real space
+  df$X <- rep(gLocs$X, y)
+  df$Y <- rep(gLocs$Y, y)
+  df$D <- rep(gLocs$D, y)
+  
+  
+  #X <- cum.r*sum(gridOut$PpSqKm)/sum(gridOut$PpSqKm*(1/gridOut$Q))
+  
+  # PPS = exp(intercept + density effect + noise) * rate change
+  df$PPS <- exp(i + (b1*d.n)[df$MOORING] + rnorm(n*y, 0, sd=sdev)) * cum.r[df$YEAR] 
+  #* (1/gO$Q[df$MOORING]) * X[df$YEAR]
+  
+  return(df)
+  
+}
+
+new.data <- simSpatial(n = nrow(gridOut), y = 10, r = -.5)
+load("./Data/coastXY.RData")
+require(animation)
+saveGIF(
+for (i in 1:10){
+p <- ggplot()+
+  ggtitle(paste("Year", i, sep=" "))+
+  geom_polygon(data=coast, aes(x=X, y=Y), fill="gray")+
+  geom_point(data=subset(new.data, YEAR==i), aes(x=X, y=Y, color=PPS), size=0.5)+
+  scale_color_gradient2(low="dodgerblue2", mid="yellow", high="red",
+                        midpoint=max(new.data$PPS)/2, limits=c(0, max(new.data$PPS)))+
+  theme_bw()+
+  coord_equal(xlim=c(-50000, 50000), ylim=c(-50000,50000))+
+  xlab("X (m)")+
+  ylab("Y (m)")+
+  theme(panel.grid.major=element_blank(), 
+        panel.grid.minor=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold", size=10),
+        
+        strip.background=element_blank(),
+        panel.margin=unit(1.25, "lines"),
+        legend.key.height=unit(c(1.25), "lines"),
+        plot.margin=unit(c(2, 0, 0, 0), "lines"),
+        legend.position=c(0.1, 0.2))+
+  theme(plot.title = element_text(face="bold"))
+print(p)
+}, movie.name="./Figures/PPS.gif")
+
+?subset
+
+# Code for testing the function
+#
+# new.data <- simSpatial(n = 50, y = 10, r = -.25)
+# 
+# new.data <- as.data.frame(new.data, names=c("MOORING", "YEAR", "DENSITY", "PPS"))
+# 
+# plot(new.data$YEAR, new.data$PPS)
+# 
+# new.model <- lme(fixed = log(PPS) ~ YEAR, 
+#                  random = ~ 1 | MOORING, data = new.data)
+# 
+# summary(new.model)
+# 
+# plot(new.model, resid(.) ~ fitted(.))
