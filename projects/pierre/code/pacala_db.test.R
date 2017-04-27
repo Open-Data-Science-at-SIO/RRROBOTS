c1733d9011c4fc1b4d0b3d34f85ec76a8bb030f7
Pierre <pchurkian@ucsd.edu>
2017-04-26 21:47:30 -0700

    .

--- projects/pierre/code/pacala_db.test.R
+++ projects/pierre/code/pacala_db.test.R
@@ -1,54 +1,43 @@
 library(testthat)
 
-# ======================= UTILITIES ==================================
-setup_test_db_with_schema_and_data = function(p_file_path, p_data_filename){
-  # DESC: load the schema, sets up the data structure and inserts the data
-  db_obj = setup_test_db_with_schema()
-  add_test_data(db_obj, p_file_path, p_data_filename)
-}
+# =================== PATHS =======================
+c_file_path = 'C:\\dev\\pacala\\data'
+c_data_file_name = 'pacala_data.txt'
+c_params_file_name = 'pacala_parameters.txt'
 
+# ======================= TEST SETUP UTILITIES ==================================
 setup_test_db_with_schema_data_and_params = function(p_file_path, p_data_filename, p_params_filename){
-  # DESC: load the schema, sets up the data structure and inserts the data & params
   db_obj = setup_test_db_with_schema()
   db_obj = add_test_data(db_obj, p_file_path, p_data_filename)
   add_test_params(db_obj, p_file_path, p_params_filename)
 }
-
-setup_test_db_with_schema = function(){
-  # DESC: load the schema and sets up the data structure
-  schema_list = load_schema()
-  do.call(setup_db, list(schema_list))
+setup_test_db_with_schema_and_data = function(p_file_path, p_data_filename){
+  db_obj = setup_test_db_with_schema()
+  add_test_data(db_obj, p_file_path, p_data_filename)
 }
-
+setup_test_db_with_schema = function(){
+  schema_list = load_schema()
+  setup_db(schema_list)
+}
 add_test_data = function(p_db, p_file_path, p_data_filename){
-  # DESC: loads data ans insert into already set up db 
   data_to_insert = load_pacala_data(p_file_path, p_data_filename)
   insert_data_into_db(p_db, data_to_insert)
 }
-
 add_test_params = function(p_db, p_file_path, p_params_filename){
-  # DESC: load params and insert into db 
   params_to_insert = load_pacala_params(c_file_path, c_params_file_name)
   insert_db_params(p_db, params_to_insert)
 }
-
-
-# =================== schema =======================
-
-c_file_path = 'C:\\dev\\pacala'
-c_data_file_name = 'pacala_data.txt'
-c_params_file_name = 'pacala_parameters.txt'
 
 # ================= DATA HANDLING ======================
 
 
 #----- setup and utils ---
-test_that('Db object - is set up', {
+test_that('Db - object is set up', {
   # setup
   source('pacala_db.R')
   schema_list = load_schema()
   # execute
-  r = do.call(setup_db, list(schema_list))
+  r = setup_db(schema_list)
   # test
   expect_is(r, 'list')
   expect_equal(length(r), 2)
@@ -62,7 +51,7 @@ test_that('Db object - is set up', {
 
 #------- inserts ------------------
 
-test_that('Db object - data are inserted', {
+test_that('Db - data are inserted', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema()
@@ -78,7 +67,7 @@ test_that('Db object - data are inserted', {
 })
 
 
-test_that('Db object - params are inserted', {
+test_that('Db - params are inserted', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_and_data(c_file_path, c_data_file_name)
@@ -94,7 +83,7 @@ test_that('Db object - params are inserted', {
 
 
 
-test_that('Db object - derived values are populated', {
+test_that('Db - derived values are populated', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_data_and_params(c_file_path, c_data_file_name, 
@@ -148,7 +137,7 @@ test_that('Db - get current data after inserting the data & params', {
 })
 
 
-test_that('species is retrieved for individual', {
+test_that('Db - species is retrieved for individual', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_and_data(c_file_path, c_data_file_name)
@@ -161,7 +150,7 @@ test_that('species is retrieved for individual', {
 
 
 #------------- properties -------------
-test_that('Db object - db length is returned', {
+test_that('Db - db length is returned', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_and_data(c_file_path, c_data_file_name)
@@ -171,7 +160,7 @@ test_that('Db object - db length is returned', {
   expect_equal(r, 4)
 })
 
-test_that('Db object - db width is returned after initializing', {
+test_that('Db - db width is returned after initializing', {
   # setup
   source('pacala_db.R')
   # load schema and setup db
@@ -182,7 +171,7 @@ test_that('Db object - db width is returned after initializing', {
   expect_equal(r, 0)
 })
 
-test_that('Db object - db width is returned after inserting data', {
+test_that('Db - db width is returned after inserting data', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_and_data(c_file_path, c_data_file_name)
@@ -192,7 +181,7 @@ test_that('Db object - db width is returned after inserting data', {
   expect_equal(r, 5)
 })
 
-test_that('Db object - db width is returned after inserting parameters', {
+test_that('Db - db width is returned after inserting parameters', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_data_and_params(c_file_path, c_data_file_name, 
@@ -204,7 +193,7 @@ test_that('Db object - db width is returned after inserting parameters', {
 })
 
 # ======================== FILES ==========================
-test_that('Data are loaded', {
+test_that('Files - Data are loaded', {
   # setup
   source('pacala_db.R')
   schema_list = load_schema()
@@ -217,7 +206,7 @@ test_that('Data are loaded', {
 })
 
 
-test_that('Parameters are loaded', {
+test_that('Files - Parameters are loaded', {
   # setup
   source('pacala_db.R')
   schema_list = load_schema()
@@ -231,7 +220,7 @@ test_that('Parameters are loaded', {
 
 # ======================== GRAPHICS =============================
 
-test_that('Landscape is drawn', {
+test_that('GRaphics - Landscape is drawn', {
   # setup
   source('pacala_db.R')
   db_obj = setup_test_db_with_schema_data_and_params(c_file_path, c_data_file_name, 
