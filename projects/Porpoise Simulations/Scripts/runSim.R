a5f64d74ad2c2d9de477c64bd48e96da564ab29a
eirenjacobson <eiren.jacobson@gmail.com>
2017-05-30 11:39:00 -0700

    Fixed bug on Line 19 of runSim (pars were not being passed to simSpatial).  Tried initial sim runs with 1000 replicates for each of 4 scenarios; tabled results.

--- projects/Porpoise Simulations/Scripts/runSim.R
+++ projects/Porpoise Simulations/Scripts/runSim.R
@@ -1,4 +1,4 @@
-
+# NOTE 05302017: prev. there was a bug; b and depth were not being passed to simSpatial in line 19 
 runSim <- function(n.moorings, p.change, nrep, b, d.min, d.max, file.out){
 
   source("./Scripts/simSpatial.R")
@@ -16,7 +16,7 @@ runSim <- function(n.moorings, p.change, nrep, b, d.min, d.max, file.out){
     
       for (i in 1:nrep){
       
-      nd <- as.matrix(simSpatial(no, 10, pc, b = 0))
+      nd <- as.matrix(simSpatial(no, 10, pc, d.min=d.min, d.max=d.max, b=b))
       
       nd <- as.data.frame(nd, names=c("MOORING", "YEAR", "DENSITY", "PPS"))
       
@@ -41,23 +41,18 @@ runSim <- function(n.moorings, p.change, nrep, b, d.min, d.max, file.out){
   save(tab.res, file=file.out)
   
 }
-  
-
-
 
 n.moorings <- c(10, 25, 50, 75, 100)
 p.change.fig <- seq(-0.5, 0.5, by = 0.05)
 p.change.table <- c(-0.5, -0.25, -0.1, 0.25)
 nrep <- 10000
 #runSim(n.moorings, p.change, nrep, b, d.min, d.max, file.out)
-runSim(n.moorings, p.change.fig, 1000, 0, file.out="./Data/simSpatialb0Results.RData")
+#runSim(n.moorings, p.change.fig, 1000, 0, file.out="./Data/simSpatialb0Results.RData")
 
-runSim(n.moorings, p.change.table, nrep, 0, file.out="./Data/simSpatialb0Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 1, file.out="./Data/simSpatialb1Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 0, d.min=0, d.max=-200, file.out="./Data/simSpatialStratb0Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 1, d.min=0, d.max=-200, file.out="./Data/simSpatialStratb1Resultsx10000.RData")
-
-
+runSim(n.moorings, p.change.table, nrep, 0, d.min=0, d.max=-1000, file.out="./Data/simSpatialb0Resultsx10000.RData")
+runSim(n.moorings, p.change.table, nrep, 1, d.min=0, d.max=-1000, file.out="./Data/simSpatialb1Resultsx10000.RData")
+runSim(n.moorings, p.change.table, nrep, 0, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb0Resultsx10000.RData")
+runSim(n.moorings, p.change.table, nrep, 1, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb1Resultsx10000.RData")
 
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73","#0072B2",  "#CC79A7")
 
