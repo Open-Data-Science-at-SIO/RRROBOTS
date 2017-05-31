64edf055719d9688eeac604d5f982dab764f9d2a
eirenjacobson <eiren.jacobson@gmail.com>
2017-05-31 17:49:00 +0000

    Simulation runs from Pseudorca for stratified/unstratified and uniform/basin-type declines

--- projects/Porpoise Simulations/Scripts/runSim.R
+++ projects/Porpoise Simulations/Scripts/runSim.R
@@ -42,17 +42,22 @@ runSim <- function(n.moorings, p.change, nrep, b, d.min, d.max, file.out){
   
 }
 
-n.moorings <- c(10, 25, 50, 75, 100)
+n.moorings <- 75
+p.change <- -0.25
 p.change.fig <- seq(-0.5, 0.5, by = 0.05)
 p.change.table <- c(-0.5, -0.25, -0.1, 0.25)
 nrep <- 10000
 #runSim(n.moorings, p.change, nrep, b, d.min, d.max, file.out)
 #runSim(n.moorings, p.change.fig, 1000, 0, file.out="./Data/simSpatialb0Results.RData")
 
-runSim(n.moorings, p.change.table, nrep, 0, d.min=0, d.max=-1000, file.out="./Data/simSpatialb0Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 1, d.min=0, d.max=-1000, file.out="./Data/simSpatialb1Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 0, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb0Resultsx10000.RData")
-runSim(n.moorings, p.change.table, nrep, 1, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb1Resultsx10000.RData")
+runSim(n.moorings, p.change, nrep, 0, d.min=0, d.max=-1000, file.out="./Data/simSpatialb0Resultsx10000_SingleCell.RData")
+require(lubridate)
+start <- now()
+runSim(n.moorings, p.change, nrep, 1, d.min=0, d.max=-1000, file.out="./Data/simSpatialb1Resultsx10000_SingleCell.RData")
+now() - start
+
+runSim(n.moorings, p.change, nrep, 0, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb0Resultsx10000_SingleCell.RData")
+runSim(n.moorings, p.change, nrep, 1, d.min=0, d.max=-100, file.out="./Data/simSpatialStratb1Resultsx10000_SingleCell.RData")
 
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73","#0072B2",  "#CC79A7")
 
